7a9fca9bf56c2204f87ee253269b9d1cbb56d01b
egruhn <egruhn@gmail.com>
2017-08-27 11:44:11 -0400

    cachematrix
    Updates made for Programming Assignment 2 on August 27, 2017.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,103 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  Programming Assignment 2 by Erich F Gruhn
+
+##Example: Caching the Mean of a Vectorless 
+##In this example we introduce the <<- operator which can be used to assign a 
+##value to an object in an environment that is different from the current 
+##environment. Below are two functions that are used to create a special object 
+##that stores a numeric vector and cache's its mean.
+
+##The first function, makeVector creates a special "vector", which is really a 
+##list containing a function to
+
+## 1.)  set the value of the vector
+## 2.)  get the value of the vector
+## 3.)  set the value of the mean
+## 4.)  get the value of the mean
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+##The following function calculates the mean of the special "vector" created 
+##with the above function. However, it first checks to see if the mean has 
+##already been calculated. If so, it gets the mean from the cache and skips the
+##computation. Otherwise, it calculates the mean of the data and sets the value 
+##of the mean in the cache via the setmean function.
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+
+##Assignment: Caching the Inverse of a Matrixless 
+##Matrix inversion is usually a costly computation and there may be some benefit 
+##to caching the inverse of a matrix rather than compute it repeatedly (there 
+##are also alternatives to matrix inversion that we will not discuss here). Your 
+##assignment is to write a pair of functions that cache the inverse of a matrix.
+
+##Write the following functions:
+        
+## 1.)  makeCacheMatrix: This function creates a special "matrix" object that  
+##      can cache its inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		inverseMatrix <- NULL
+        setMatrix <- function(y) {
+                x <<- y
+                inverseMatrix <<- NULL
+        }
+        
+        getMatrix <- function() x 
+        setInverse <- function(inverse) inverseMatrix <<- inverse 
+        getInverse <- function() inverseMatrix
+        list(setMatrix = setMatrix, 
+             getMatrix = getMatrix,
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## 2.)  cacheSolve: This function computes the inverse of the special "matrix" 
+##      returned by makeCacheMatrix above. If the inverse has already been 
+##      calculated (and the matrix has not changed), then the cachesolve should 
+##      retrieve the inverse from the cache.
+
+##Computing the inverse of a square matrix can be done with the solve function 
+##in R. For example, if X is a square invertible matrix, then solve(X) returns 
+##its inverse.
+
+##For this assignment, assume that the matrix supplied is always invertible.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverseMatrix <- x$getInverse()
+        if(!is.null(inverseMatrix)) { 
+                message("getting cached inverse matrix") 
+                return(inverseMatrix) 
+        }
+        MatrixData <- x$getMatrix() 
+        inverseMatrix <- solve(MatrixData, ...) 
+        x$setInverse(inverseMatrix) 
+        return(inverseMatrix) 
 }
